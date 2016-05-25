b619c67ebe283dc2740415c882d52e072178be17
Joel Torstensson <oed3933@gmail.com>
2016-05-25 16:14:08 +0200

    State graph can now find neighbours (naively).

--- StateGraph.ts
+++ StateGraph.ts
@@ -24,30 +24,45 @@ class StateNode {
         }
         return 0
     }
+
+    clone() {
+        var tmp = new StateNode(null)
+        tmp.data = []
+        for (var i = 0; i < this.data.length; i++) {
+            tmp.data[i] = this.data[i].slice(0)
+        }
+        return tmp
+    }
 }
 
 
 class StateGraph implements Graph<StateNode> {
 
-    constructor(
-        private startState : StateNode
-    ) {
-    }
-
-    outgoingEdges(node : StateNode) {
-        return null
+    outgoingEdges(node : StateNode) : Edge<StateNode>[] {
+        var edges : Edge<StateNode>[] = []
+        for (var i = 0; i < node.data.length; i++) {
+            for (var j = 0; j < node.data.length; j++) {
+                if (i==j) { continue }
+                var next = node.clone()
+                next.data[j].push(next.data[i].pop())
+                if (isValidState(next)) {
+                    edges.push({
+                        from: node,
+                        to: next,
+                        cost: 1
+                    })
+                }
+            }
+        }
+        return edges
     }
 
     compareNodes(a : StateNode, b : StateNode) {
         return a.compareTo(b)
     }
-
-    getStartingNode() : StateNode {
-        return this.startState
-    }
 }
 
-function isValidState(state : StateNode) {
+function isValidState(state : StateNode) : boolean {
     // TODO - this should check that physical laws are obeyed
     return true
 }
